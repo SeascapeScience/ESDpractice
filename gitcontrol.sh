c3bb3783813aa5cc850114d9c9ebb915eec7a98a
kirstenjohnston <73840673+kirstenjohnston@users.noreply.github.com>
2023-10-30 14:39:17 -0400

    add gitcontrol shellfile

---
+++ gitcontrol.sh
@@ -0,0 +1,10 @@
+#please change script to file path where ESD repo is stored locaally 
+cd C:\Users\kjohnston\Documents\GitHub\ESDpractice
+# pull repo to make sure everything is up to date
+git pull
+#add any files you want to add
+git add gitcontrol.sh
+# add a comment
+git commit -a -m "comment"
+# push to github
+git push
